cc0c4541539c4b20477a0b5e45e58a1efa96ce7c
Patbodin Intajak <patbodin30443@hotmail.com>
2021-10-13 13:31:04 +0700

    Basically construct test APIs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/api/v1/testtext')
+  getTestText(): any {
+    return { msg: 'Hello World!!' };
+  }
+
+  @Get('/api/v2/testtext')
+  getTestText_2(): {} {
+    return { msg: 'Hello World!! Version 2' };
+  }
+
+  @Get('/api/v3/testtext')
+  getTestText_3(): { msg: string } {
+    return { msg: 'Hello World!! Version 3' };
+  }
+
+  @Get('/api/v4/testtext')
+  @Header('Content-Type', 'text/html')
+  getTestText_4(): {} {
+    return { msg: 'Hello World!! Version 4' };
   }
 }
