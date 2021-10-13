486039575893cfd1de7dc6462c1516a13a9bcae9
Patbodin Intajak <patbodin30443@hotmail.com>
2021-10-13 13:12:57 +0700

    Successfully installed & imported chalk lib

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as chalk from 'chalk';
+
+const serverPort = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(serverPort);
+
+  console.log(chalk.blue(`This server has started at port ${chalk.green.bold(serverPort)}`));
 }
 bootstrap();
